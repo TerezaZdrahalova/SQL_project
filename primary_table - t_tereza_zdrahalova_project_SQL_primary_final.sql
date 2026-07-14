013b601d41cea0f1386f753fc53739ec97114461
TerezaZdrahalova <orsagte@centrum.cz>
2026-07-14 16:57:58 +0200

    Update primary_table - t_tereza_zdrahalova_project_SQL_primary_final.sql

--- primary_table - t_tereza_zdrahalova_project_SQL_primary_final.sql
+++ primary_table - t_tereza_zdrahalova_project_SQL_primary_final.sql
@@ -4,28 +4,28 @@
 DROP TABLE IF EXISTS t_Tereza_Zdrahalova_project_SQL_primary_final;
 CREATE TABLE t_tereza_zdrahalova_project_sql_primary_final AS 
 WITH wages AS (									
-	SELECT													                -- příprava mzdových tabulek
+	SELECT													                -- Příprava mzdových tabulek.
 		payroll_year,
 		industry_branch_code,
 		AVG(value) AS avg_wage
 	FROM czechia_payroll 
-	WHERE value_type_code = 5958							      			-- průměrná hrubá mzda na zaměstnance
-		AND calculation_code = 200							     			-- přepočet
-		AND payroll_year BETWEEN 2006 AND 2018								-- filtr roků, kde mají mzdy i ceny společná data
+	WHERE value_type_code = 5958							      			-- Průměrná hrubá mzda na zaměstnance.
+		AND calculation_code = 200							     			-- Přepočet.
+		AND payroll_year BETWEEN 2006 AND 2018								-- Filtr roků, kde mají mzdy i ceny společná data.
 	GROUP BY payroll_year, industry_branch_code
 ),
 prices AS (
-	SELECT													                -- příprava cenových dat 
+	SELECT													                -- Příprava cenových dat. 
 		EXTRACT (YEAR FROM date_from) AS price_year,
 		avg (CASE WHEN category_code = 114201 THEN value END) AS milk_price,
 		avg (CASE WHEN category_code = 111301 THEN value END) AS bread_price
 	FROM czechia_price 
 	WHERE category_code IN (114201, 111301)
-		AND region_code IS NULL 							       		 	-- celorepublikový průměr
+		AND region_code IS NULL 							       		 	-- Celorepublikový průměr.
 		AND EXTRACT (YEAR FROM date_from) BETWEEN 2006 AND 2018
 	GROUP BY price_year
 )
-	SELECT 													                -- spojení mezd a cen do jedné tabulky
+	SELECT 													                -- Spojení mezd a cen do jedné tabulky.
 		wages.payroll_year,
 		wages.industry_branch_code,
 		ibranch.name AS industry_name,
